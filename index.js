df11f42900ef954c0078fe88cb841f99aa4f48cc
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-03-21 06:38:30 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    const { quote, attributed, source} = req.body;
+    res.render('index', {quote, attributed, source});
+});
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+module.exports = router;
